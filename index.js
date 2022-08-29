bbcb0178e7743ad0eb3a1e5c4c8bec4eec5f43df
Junrey Lañas <jrydev404@gmail.com>
2022-08-29 15:08:42 +0800

    configure rtk store

---
+++ index.js
@@ -0,0 +1,14 @@
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+
+console.log('Initial state:', store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated state:', store.getState());
+})
+
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.ordered())
+store.dispatch(cakeActions.restocked(3))
+
+unsubscribe()
